9492a7ed9a67ef5680b0888e14fabbe1f4e38f83
witschis <samira.witschi@ict-campus.net>
2024-05-08 12:02:49 +0200

    removed lines for testing (env)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import styles from "@/styles/Home.module.css";
-import { Env } from "@humanwhocodes/env";
+//import { Env } from "@humanwhocodes/env";
 
 export default function Home({ items }) {
   return (
@@ -15,9 +15,9 @@ export default function Home({ items }) {
 }
 
 export async function getStaticProps() {
-  const env = new Env();
+  //const env = new Env();
 
-  console.log("ENV:", env.get("FUNC_URL"));
+  //console.log("ENV:", env.get("FUNC_URL"));
 
   const res = await fetch("https://witschis-funcs.azurewebsites.net/api/items");
 
